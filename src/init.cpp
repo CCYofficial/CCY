b32f17ad5b37076fa70aea751b4690a1bf8da613
Team CCY <54861297+CCYofficial@users.noreply.github.com>
2019-10-02 18:18:41 +0200

    1.1.0.0

--- src/init.cpp
+++ src/init.cpp
@@ -512,6 +512,8 @@ std::string LicenseInfo()
            "\n" +
            FormatParagraph(strprintf(_("Copyright (C) 2014-%i The Dash Developers"), COPYRIGHT_YEAR)) + "\n" +
            "\n" +
+           FormatParagraph(strprintf(_("Copyright (C) 2015-%i The PIVX Core Developers"), COPYRIGHT_YEAR)) + "\n" +
+           "\n" +
            FormatParagraph(strprintf(_("Copyright (C) 2018-%i The Cryptocurrency Developers"), COPYRIGHT_YEAR)) + "\n" +
            "\n" +
            FormatParagraph(_(" ")) + "\n" +
