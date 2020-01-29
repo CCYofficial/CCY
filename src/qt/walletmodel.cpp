de524c3e3dc345c9206e63c3cc2d68bdc5b0ff23
Team CCY <54861297+CCYofficial@users.noreply.github.com>
2020-01-29 21:34:38 +0100

    1.2.0.0

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -87,6 +87,11 @@ CAmount WalletModel::getImmatureBalance() const
     return wallet->GetImmatureBalance();
 }
 
+CAmount WalletModel::getLockedBalance() const
+{
+    return wallet->GetLockedCoins();
+}
+
 bool WalletModel::haveWatchOnly() const
 {
     return fHaveWatchOnly;
