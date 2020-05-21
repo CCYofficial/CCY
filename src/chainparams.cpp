6fc8933f225180ebabb7317221aaf9c5a55da5c4
Team CCY <54861297+CCYofficial@users.noreply.github.com>
2020-05-21 14:03:30 +0200

    new checkpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -149,6 +149,7 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
 	(157000, uint256("0x9c79e75a835990aa0a5f9cac2eadd9a801d4c11aa4875707fb918726591a0c48"))
 	(158000, uint256("0xedb53d9a536ba3b7e8616552c12f6055e3e58b6efcf0418adb9e2c8fb753f0b3"))
 	(159000, uint256("0xb562c2a39abffc903a398e9bf1d6b4dce09e063cca2473fbeac875d30452b136"))
+	(160000, uint256("0xe3a14ede76cba033cd476ad09e88feff6fedfb3905828e8bfb273030459b7ee6"))
 	;
 
 
