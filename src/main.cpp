a736d571ca398fbf9a41075f5f85de9db5cd2691
Team CCY <54861297+CCYofficial@users.noreply.github.com>
2020-08-31 21:27:39 +0200

    sync fix

--- src/main.cpp
+++ src/main.cpp
@@ -3327,7 +3327,7 @@ bool CheckWork(const CBlock block, CBlockIndex* const pindexPrev)
     if (block.nBits != nBitsRequired)
         return error("%s : incorrect proof of work at %d", __func__, pindexPrev->nHeight + 1);
 
-    if (block.IsProofOfStake() && (pindexPrev->nHeight > 231100) && !IsInitialBlockDownload()) {
+    if (block.IsProofOfStake() && (pindexPrev->nHeight > 234600) && !IsInitialBlockDownload()) {
         uint256 hashProofOfStake;
         uint256 hash = block.GetHash();
 
